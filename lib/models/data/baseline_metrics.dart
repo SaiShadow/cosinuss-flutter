bd7735ba58281c99508b6019a3637086e0fac9db
SaiShadow <92605941+SaiShadow@users.noreply.github.com>
2025-01-11 20:31:15 +0100

    store baseline measurements in a class

---
+++ lib/models/data/baseline_metrics.dart
@@ -0,0 +1,46 @@
+import 'package:cosinuss/models/data/session_data.dart';
+
+class BaselineMetrics {
+  final double averageHeartRate;
+  final double averageBodyTemperature;
+  final double averageAccX, averageAccY, averageAccZ;
+
+  BaselineMetrics({
+    required this.averageHeartRate,
+    required this.averageBodyTemperature,
+    required this.averageAccX,
+    required this.averageAccY,
+    required this.averageAccZ,
+  });
+
+  /// Calculate baseline metrics from a list of `SessionData`
+  factory BaselineMetrics.fromSessionData(List<SessionData> sessionData) {
+    final int count = sessionData.length;
+
+    final double avgHeartRate =
+        sessionData.map((data) => data.heartRate).reduce((a, b) => a + b) /
+            count;
+
+    final double avgBodyTemperature = sessionData
+            .map((data) => data.bodyTemperature)
+            .reduce((a, b) => a + b) /
+        count;
+
+    final double avgAccX =
+        sessionData.map((data) => data.accX).reduce((a, b) => a + b) / count;
+
+    final double avgAccY =
+        sessionData.map((data) => data.accY).reduce((a, b) => a + b) / count;
+
+    final double avgAccZ =
+        sessionData.map((data) => data.accZ).reduce((a, b) => a + b) / count;
+
+    return BaselineMetrics(
+      averageHeartRate: avgHeartRate,
+      averageBodyTemperature: avgBodyTemperature,
+      averageAccX: avgAccX,
+      averageAccY: avgAccY,
+      averageAccZ: avgAccZ,
+    );
+  }
+}
