6369d86e01657141ecea2350cd47d8f4ed5310f2
SaiShadow <92605941+SaiShadow@users.noreply.github.com>
2025-01-14 10:40:08 +0100

    Update pomodoro_session_type.dart

--- lib/models/pomodoro_session_type.dart
+++ lib/models/pomodoro_session_type.dart
@@ -1,1 +1,12 @@
-enum Session { work, shortBreak }
+/// An enumeration representing the types of sessions in the Pomodoro Timer.
+///
+/// Each session type has a specific purpose and duration.
+enum Session {
+  /// Work session:
+  /// Focused time for productivity, where tasks or goals are actively worked on.
+  work,
+
+  /// Short Break session:
+  /// A brief rest period to recharge, helping to maintain focus and reduce fatigue before the next work session.
+  shortBreak,
+}
