dbd089c68f55fa58e785f46cf7b3552eb1d5361f
SaiShadow <92605941+SaiShadow@users.noreply.github.com>
2025-01-14 12:22:37 +0100

    Update sensor_display_widget.dart

--- lib/widgets/sensor_display_widget.dart
+++ lib/widgets/sensor_display_widget.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
+/// A widget that displays sensor data such as focus level, stress level,
+/// heart rate, and body temperature in a formatted and visually appealing layout.
 class SensorDataWidget extends StatelessWidget {
+  /// Focus level as a string.
   final String focusLevel;
+
+  /// Stress level as a string.
   final String stressLevel;
+
+  /// Heart rate as a string.
   final String heartRate;
+
+  /// Body temperature as a string.
   final String temperature;
 
+  /// Constructs a `SensorDataWidget` with the required sensor data.
+  ///
+  /// Parameters:
+  /// - [focusLevel]: Current focus level.
+  /// - [stressLevel]: Current stress level.
+  /// - [heartRate]: Current heart rate.
+  /// - [temperature]: Current body temperature.
   const SensorDataWidget({
     Key? key,
     required this.focusLevel,
@@ -14,9 +30,13 @@ class SensorDataWidget extends StatelessWidget {
     required this.temperature,
   }) : super(key: key);
 
+  /// Builds the widget tree for displaying sensor data.
+  ///
+  /// Returns a container with a structured display of the focus level, stress level,
+  /// heart rate, and body temperature, each with corresponding icons and labels.
   @override
   Widget build(BuildContext context) {
-    const double gapSize = 20;
+    const double gapSize = 20; // Space between data sections.
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       padding: const EdgeInsets.all(12.0),
@@ -39,6 +59,14 @@ class SensorDataWidget extends StatelessWidget {
     );
   }
 
+  /// Helper method to build an individual data section.
+  ///
+  /// Parameters:
+  /// - [title]: Title of the section (e.g., "Focus").
+  /// - [value]: The value of the data to be displayed.
+  /// - [icon]: An icon representing the data.
+  ///
+  /// Returns a `Row` widget containing the icon, title, and value.
   Widget _buildDataSection(String title, String value, IconData icon) {
     return Row(
       mainAxisSize: MainAxisSize
